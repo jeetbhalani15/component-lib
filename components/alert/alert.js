794bcd697e159c8ca4dad04b8d27e145035898c9
jeetbhalani15 <Jeetbhalani@gmail.com>
2022-02-01 19:28:44 +0530

    added alert.js

---
+++ components/alert/alert.js
@@ -0,0 +1,7 @@
+const alertBox= document.querySelector('.alert-btn');
+const btn = document.querySelector('.close-btn');
+
+btn.addEventListener('click', ()=>{
+    alertBox.style.display="none"
+})
+console.log("hello")
